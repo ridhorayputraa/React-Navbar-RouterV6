3a95775290ae833018142f36850593610f040a0d
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-06 20:20:00 +0700

    set Route for Link

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import {
 import Home from "./pages/Home";
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Services from './pages/Service'
+import Skilss from './pages/Skils'
+
 
 function App() {
   return (
@@ -21,6 +24,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} exact></Route>
           <Route path="/about" element={<About/>} exact></Route>
+          <Route path="/services" element={<Services/>} exact></Route>
+          <Route path="/skilss" element={<Skilss/>} exact></Route>
           <Route path="/contact" element={<Contact/>} exact></Route>
         </Routes>
       </Router>
